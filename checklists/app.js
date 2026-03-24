1c6585e71977340b739f8ead493f30df77b5a106
ChasingMajors <john@chasingmajors.com>
2026-03-24 10:19:15 -0600

    Update app.js

--- checklists/app.js
+++ checklists/app.js
@@ -17,7 +17,7 @@
 ========================================= */
 
 // ---------------- CONFIG ----------------
-const EXEC_URL = "https://script.google.com/macros/s/AKfycbz_bnQtHNUd33PYq2kwZPJKI7e_dryN-Ra0sXlkoOr0-QQl9ZRuX7n7G8otbQUkKzU5/exec";
+const EXEC_URL = "https://script.google.com/macros/s/AKfycbzHnkUSmmgaoTodQQ8xBUj2PIDSsn7BC6CihZl0nO5iV6gcHMldhcbGzx_nBICGZM28/exec";
 
 const INDEX_KEY = "cv_index_v1";
 const INDEX_VER_KEY = "cv_index_ver_v1";
